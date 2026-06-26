dbe4f92ef878c4eba39cbe2e6bfa501d56762082
spandan-appointy <spandan@appointy.com>
2026-06-26 13:21:18 +0530

    fix: validate protobuf v2 duration and timestamp templates

--- templates/go/timestamp.go
+++ templates/go/timestamp.go
@@ -5,8 +5,8 @@ const timestampTpl = `{{ $f := .Field }}{{ $r := .Rules }}
 
 	{{ if or $r.Lt $r.Lte $r.Gt $r.Gte $r.LtNow $r.GtNow $r.Within $r.Const }}
 		if t := {{ accessor . }}; t != nil {
-			ts, err := ptypes.Timestamp(t)
-			if err != nil { return {{ errCause . "err" "value is not a valid timestamp" }} }
+			if err := t.CheckValid(); err != nil { return {{ errCause . "err" "value is not a valid timestamp" }} }
+			ts := t.AsTime()
 
 			{{ template "timestampcmp" . }}
 		}
